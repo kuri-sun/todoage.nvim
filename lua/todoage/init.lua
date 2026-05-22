105ff6f8289e6456ffa92242c3fba6d54f335e19
kuri-sun <harukikuriwada@gmail.com>
2026-05-22 17:17:41 +0900

    feat: annotate lines containing TODO

--- lua/todoage/init.lua
+++ lua/todoage/init.lua
@@ -5,16 +5,15 @@ local M = {}
 function M.refresh()
 	vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
 
-	vim.api.nvim_buf_set_extmark(
-		0, -- 0 for current buffer
-		ns, -- the namespace id from create_namespace
-		0, -- 0-indexed!! line 1 = 0
-		0, -- 0-indexed; for end-of-line virt_text, 0 is fine
-		{
-			virt_text = { { "(hardcoded)", "Comment" } },
-			virt_text_pos = "eol",
-		}
-	)
+	local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+	for i, line in ipairs(lines) do
+		if line:find("%f[%w_]TODO%f[%W_]") then
+			vim.api.nvim_buf_set_extmark(0, ns, i - 1, 0, {
+				virt_text = { { "(found)", "Comment" } },
+				virt_text_pos = "eol",
+			})
+		end
+	end
 end
 
 return M
